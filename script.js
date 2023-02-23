a247afb27da716d413f5ea3b8babb2bd9f836106
Noor Chasib <noorchasib@gmail.com>
2023-02-23 14:09:57 -0800

    create larva class

--- script.js
+++ script.js
@@ -207,7 +207,11 @@ window.addEventListener("load", function () {
 		update() {
 			this.spriteX = this.collisionX - this.width * 0.5;
 			this.spriteY = this.collisionY - this.height * 0.5 - 30;
-			let collisionObjects = [this.game.player, ...this.game.obstacles, ...this.game.enemies];
+			let collisionObjects = [
+				this.game.player,
+				...this.game.obstacles,
+				...this.game.enemies,
+			];
 			collisionObjects.forEach((object) => {
 				let [
 					collision,
@@ -223,6 +227,31 @@ window.addEventListener("load", function () {
 					this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
 				}
 			});
+		}
+	}
+
+	class Larva {
+		constructor(game, x, y) {
+			this.game = game;
+			this.collisionX = x;
+			this.collisionY = y;
+			this.collisionRadius = 30;
+			this.image = document.getElementById("larva");
+			this.spriteWidth = 150;
+			this.spriteHeight = 150;
+			this.width = this.spriteWidth;
+			this.height = this.spriteHeight;
+			this.spriteX;
+			this.spriteY;
+			this.speedY = 1 + Math.random();
+		}
+		draw(context) {
+			context.drawImage(this.image, this.spriteX, this.spriteY);
+		}
+		update() {
+			this.collisionY -= this.speedY;
+			this.spriteX = this.collisionX - this.width * 0.5;
+			this.spriteY = this.collisionY - this.height * 0.5;
 		}
 	}
 
